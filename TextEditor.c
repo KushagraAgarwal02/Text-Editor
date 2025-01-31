72abfdbd3ce738b3d29249c05ada734fba6ad16b
Kushagra Agarwal <149239839+KushagraAgarwal02@users.noreply.github.com>
2025-01-31 19:57:37 +0530

    Update TextEditor.c

--- TextEditor.c
+++ TextEditor.c
@@ -1,34 +1,41 @@
-// Text Editor
-
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
-// Global functions and variables
-FILE *file;
-char filename[20];
 void create();
-void edit();
+void entertxt();
 void display();
 void merge();
 void status();
 void find();
+void print_status();
+void open();
+void edit();
 
+FILE *file;
+char filename[20];
+int ch;
+int lines = 1, words = 0, characters = 0, paragraphs = 1;
+char text[1000];
 
 int main() {
     int choice;
-    printf("\n\t\t\t\t\t\t\t\t\t\t\t***** TEXT EDITOR *****");
+
+    printf("\n\n\t\t*************************************************************
+WELCOME ********************************************************");
+
     do {
         printf("\n\n\tMENU:\n\n ");
-        printf("\n\t1.CREATE\n\t2.DISPLAY\n\t3.MERGE\n\t4.STATUS\n\t5.FIND\n\t0.EXIT\n");
+        printf("\n\t1.CREATE\n\t2.OPEN\n\t3.MERGE\n\t4.STATUS\n\t5.FIND\n\t0.EXIT\n");
         printf("\n\tEnter your choice: ");
         scanf("%d", &choice);
+
         switch (choice) {
             case 1:
                 create();
                 break;
             case 2:
-                display();
+                open();
                 break;
             case 3:
                 merge();
@@ -40,7 +47,8 @@ int main() {
                 find();
                 break;
             case 0:
-                printf("Exiting the program.\n");
+                printf("\n\n\t\t\t\t\t********************************EXITING THE 
+PROGRAM.********************************");
                 exit(0);
                 break;
             default:
@@ -51,27 +59,22 @@ int main() {
 
 void create() {
     int choice;
-    // create()
     printf("\nENTER FILE NAME and its .extension: ");
     scanf("%s", filename);
     file = fopen(filename, "w+");
-
     if (file != NULL) {
         printf("File %s created successfully.\n", filename);
-
         do {
-            // Sub-Menu
             printf("\nENTER OPERATION FROM THE FOLLOWING:");
             printf("\n1. EDIT\n2. DISPLAY\n0. EXIT\n");
             printf("\nENTER YOUR CHOICE: ");
             scanf("%d", &choice);
-
             switch (choice) {
                 case 1:
-                    edit();
+                    entertxt();
                     break;
                 case 2:
-                    display();
+                    display(filename);
                     break;
                 case 0:
                     break;
@@ -79,70 +82,51 @@ void create() {
                     printf("INCORRECT CHOICE!\n");
             }
         } while (choice != 0);
-
         fclose(file);
     } else {
         printf("Error opening the file.\n");
     }
 }
 
-void edit() {
-    char text[1000];
-    file = fopen(filename, "a+");  // Open the file in append mode
+void entertxt() {
+    file = fopen(filename, "a+");
     printf("Enter text (type 'END' on a new line to save and exit):\n");
-
     while (fgets(text, sizeof(text), stdin)) {
         if (strcmp(text, "END\n") == 0) {
-            break;  // Exit the input loop when "END" is entered
+            break;
         }
         fputs(text, file);
     }
-
-    fclose(file);  // Close the file after editing
+    fclose(file);
 }
 
-
-void display() {
-    file = fopen(filename, "r");  // Open the file in read mode
+void display(char n[20]) {
+    file = fopen(n, "r");
     if (file == NULL) {
         printf("ERROR\n");
         return;
     }
-
-    printf("FILENAME: %s\nTEXT:\n", filename);
-    
+    printf("FILENAME: %s\nTEXT:\n", n);
     char text[1000];
     while (fgets(text, sizeof(text), file)) {
-        printf("%s", text);  // Display each line of the file
+        printf("%s", text);
     }
-
     fclose(file);
 }
 
-void status(){
-    FILE *file;
-    char filename[100];
-    char ch;
-    int lines = 0, words = 0, characters = 0, paragraphs = 0;
-    int last_char_was_space = 1;
-    int last_char_was_newline = 1;
-    // Input path of files to count lines and words
+void status() {
     printf("Enter file name: ");
     scanf("%s", filename);
-
-    // Open file
     file = fopen(filename, "r");
-
-    // Check if file exists
     if (file == NULL) {
         printf("Could not open file %s", filename);
         return;
     }
-
-    // Extract characters from file and count lines, words and characters
+    int last_char_was_space = 1;
+    int last_char_was_newline = 1;
+    int last_char_was_word = 0;
     while ((ch = fgetc(file)) != EOF) {
         characters++;
-
         if (ch == '\n') {
             lines++;
             if (last_char_was_newline) {
@@ -152,72 +136,106 @@ void status(){
         } else {
             last_char_was_newline = 0;
         }
-
         if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') {
             last_char_was_space = 1;
-        } else if (last_char_was_space) {
-            words++;
+            if (last_char_was_word) {
+                words++;
+                last_char_was_word = 0;
+            }
+        } else {
             last_char_was_space = 0;
+            last_char_was_word = 1;
         }
     }
-
-    fclose(file);
-
-    printf("Lines: %d\n", lines+1);
-    printf("Words: %d\n", words);
-    printf("Characters: %d\n", characters);
-    printf("Paragraphs: %d\n", paragraphs);
-
+    if (last_char_was_word) {
+        words++;
+    }
+    if (characters > 0) {
+        lines++;
+    }
+    print_status();
 }
 
-void merge(){
-    FILE *file1, *file2, *file3;
-    char ch,f1[10],f2[10],f3[10];
+void print_status() {
+    printf("Lines: %d\n", lines);
+    printf("Words: %d\n");
+    printf("Paragraphs: %d\n", paragraphs);
+}
 
-    // Open first file for reading
+void merge() {
+    FILE *file1, *file2, *file3;
+    char ch, f1[20], f2[20], f3[20];
     printf("Enter name of file 1:");
-    scanf("%s",f1);
+    scanf("%s", f1);
     file1 = fopen(f1, "r");
     if (file1 == NULL) {
-        printf("Cannot open file1.txt\n");
+        printf("Cannot open %s\n", f1);
         return;
     }
-
-    // Open second file for reading
-    printf("Enter name of file you want to merge:");
-    scanf("%s",f2);
+    printf("Enter name of file 2:");
+    scanf("%s", f2);
     file2 = fopen(f2, "r");
     if (file2 == NULL) {
-        printf("Cannot open file2.txt\n");
+        printf("Cannot open %s\n", f2);
         return;
     }
-
-    // Open third file for writing
     printf("Enter name of file 3:");
-    scanf("%s",f3);
+    scanf("%s", f3);
     file3 = fopen(f3, "w");
     if (file3 == NULL) {
-        printf("Cannot open file3.txt\n");
+        printf("Cannot open %s\n", f3);
         return;
     }
-
-    // Read contents from first file and write to third file
     while ((ch = fgetc(file1)) != EOF)
         fputc(ch, file3);
-
-    // Read contents from second file and write to third file
     while ((ch = fgetc(file2)) != EOF)
         fputc(ch, file3);
-
-    printf("Merged file1.txt and file2.txt into file3.txt\n");
-
-    // Close all files
+    printf("\t\t\t\t\t\t\t\t\tMERGE COMPLETE!\n\n");
     fclose(file1);
     fclose(file2);
     fclose(file3);
+    display(f3);
 }
 
-void find(){
-    
+void find() {
+    char find[1000];
+    printf("CASE SHOULD MATCH!\n");
+    printf("FIND: ");
+    scanf("%s", find);
+    printf("\nEnter file name: ");
+    scanf("%s", filename);
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Could not open file %s", filename);
+        return;
+    }
+    int found = 0;
+    int line_number = 1;
+    while (fgets(text, sizeof(text), file)) {
+        line_number++;
+        if (strstr(text, find) != NULL) {
+            printf("\nTEXT: %s FOUND AT Line %d:\n", find - 1, line_number - 1);
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("\nTEXT NOT FOUND!");
+    }
+    fclose(file);
+}
+
+void open() {
+    char nameoffile[20];
+    printf("NAME OF FILE TO OPEN:");
+    scanf("%s", nameoffile);
+    file = fopen(nameoffile, "r+");
+    if (file == NULL) {
+        printf("ERROR! FILE DOES NOT EXIST!\n");
+        return;
+    }
+    printf("FILENAME: %s\nTEXT:\n", nameoffile);
+    while (fgets(text, sizeof(text), file)) {
+        printf("%s", text);
+    }
 }
 
